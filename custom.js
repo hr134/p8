43f58425a6929102198b5ca971ba1d35ec021d4c
hr134 <107949496+hr134@users.noreply.github.com>
2022-07-18 04:18:40 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,136 @@
+
+var a = 1;
+var b = 7;
+
+var total = a + b;
+
+
+
+console.log(a+b);
+
+var a = 10;
+var b = 7;
+
+
+if (a <= 10){
+    console .log(a+b);
+}
+else {
+    console.log('a is not equal to condition')
+}
+
+
+if (a <= 12){
+    console .log(a+b);
+}
+else {
+    console.log('A is not equal to condition')
+}
+
+
+///////////
+
+var number = 78;
+
+
+if (number >= 80)
+{
+    console.log('A+');
+}
+
+else if (number <= 70)
+{
+    console.log('A');
+}
+
+
+else if (number <= 60)
+{
+    console.log('A-');
+}
+
+else if (number <= 50)
+{
+    console.log('B');
+}
+
+else if (number <= 40)
+{
+    console.log('C');
+}
+
+else if (number <= 33)
+{
+    console.log('F');
+}
+//
+
+
+
+
+
+
+
+/////////////
+
+
+var user_name = prompt('Hello, what is your name?');
+if (user_name)
+{
+    alert('Hi '  + user_name +   '! wecome in our website.');
+}
+
+
+///////////
+
+
+
+var number = prompt('What was the number of your ICT subject? - in HSC?');
+
+if (number > 80)
+{
+    alert('So- you got A+ in your HSC examination!');
+    alert('Thats great! welcome for your brillient success man.');
+}
+
+else if (number  > 70)
+{
+    alert('So, you got A in your HSC examination!');
+    alert('Thats great! welcome for your success.');
+}
+
+
+else if (number  > 60)
+{
+    alert('So, you got A- in your HSC examination!');
+    alert('Its ok, try your best.');
+}
+
+else if (number  > 50)
+{
+    alert('So, you got B in your HSC examination! ');
+    alert('Its ok, *Try the best for your next time    :)');
+}
+
+else if (number > 40 )
+{
+    alert('So, you got C in your HSC examination!');
+    alert('Poor!');
+    alert('Its ok, Dont worry. *Try your best for the next time and yes, I know you can do it.');
+}
+
+else if (number > 32  )
+{
+    alert('So, you got D in your HSC examination!');
+    alert('Ow no! poor marks.. ')
+    alert('Its ok, Dont worry. be sencere in your study. *Try your best for the next time')
+    alert('And yes, I know you can do it. :)')
+}
+
+else if (number > 0  )
+{
+    alert('Ow my gosh! you were fail in your HSC examination!');
+    alert('Thats not good! You are not  sencere in your study. Pleae be sencere.')
+}
+
+
